a6f2418495af518629035b97e5e81fa95ce7b29a
ricardocmduarte <ricnewline@gmail.com>
2023-06-23 20:36:41 -0300

    Projeto do Sistema de Controle de Clientes.

---
+++ Pessoa_Fisica.cs
@@ -0,0 +1,8 @@
+namespace Atividade_07a_Sistema_de_Controle_de_Clientes
+{
+    class Pessoa_Fisica : Clientes
+    {
+        public string cpf {get; set;}
+        public string rg {get; set;}
+    }
+}
